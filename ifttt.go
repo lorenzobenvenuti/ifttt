32503d747c3a8bd8f544812a50157685d03fafcf
Lorenzo Benvenuti <lorenzo.benvenuti@gmail.com>
2016-10-15 16:44:41 +0200

    First commit

---
+++ ifttt.go
@@ -0,0 +1,39 @@
+package ifttt
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const iftttUrlTemplate string = "https://maker.ifttt.com/trigger/%s/with/key/%s"
+
+type IftttClient interface {
+	Trigger(event string, values []string)
+}
+
+type iftttClientImpl struct {
+	key string
+}
+
+func (ifttt *iftttClientImpl) Trigger(event string, values []string) {
+	if len(values) > 3 {
+		panic("IFTTT supports at most 3 values")
+	}
+	body := new(bytes.Buffer)
+	m := make(map[string]string)
+	for i, v := range values {
+		m[fmt.Sprintf("value%d", i+1)] = v
+	}
+	json.NewEncoder(body).Encode(m)
+	_, err := http.Post(fmt.Sprintf(
+		iftttUrlTemplate, event, ifttt.key), "application/json", body)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewIftttClient(key string) IftttClient {
+	return &iftttClientImpl{key}
+}
